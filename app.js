7450f0b6473c5924215cdfb344268014e79b6b1f
gooko <takakihashimotogoo@gmail.com>
2025-11-24 20:46:48 -0800

    complete jquery exercise

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 $(function() {
   // YOUR CODE HERE
+  $("#toggleButton").on("click", function(){
+    $("#myParagraph").toggle()
+  })
+})
+
+$(document).ready(function(){
+  $("#colorButton").on("click", function() {
+    $("#colorDiv").css({
+      backgroundColor: "yellow"
+    })
+  })
+
+  $("#addClassButton").on("click", function() {
+    $("#classDiv").addClass("newClass")
+  })
+  
+  $("#removeClassButton").on("click", function() {
+    $("#classDiv").removeClass("newClass")
+  })
+
+  $("#fadeInButton").on("click", function() {
+    $("#fadeDiv").fadeIn()
+  })
+
+  $("#fadeOutButton").on("click", function() {
+    $("#fadeDiv").fadeOut()
+  })
+
+  $("#slideUpButton").on("click", function() {
+    $("#slideDiv").slideUp();
+  })
+
+  $("#slideDownButton").on("click", function() {
+    $("#slideDiv").slideDown();
+  })
+
 })
